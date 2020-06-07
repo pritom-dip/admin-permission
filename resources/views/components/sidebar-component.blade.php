9b779778b47412b4f4e60d27af976949962fadd9
pritomdip <pritomdip010@gmail.com>
2020-06-07 23:24:45 +0600

    issue solved for single menu

--- resources/views/components/sidebar-component.blade.php
+++ resources/views/components/sidebar-component.blade.php
@@ -1,6 +1,6 @@
 @foreach($sideMenus as $sideMenu)
 
-@if(!empty($sideMenu->allChildrenMenus))
+@if(!empty($sideMenu->allChildrenMenus) && $sideMenu->allChildrenMenus->count() > 0 )
 
 <li class="treeview">
     <a href="#">
@@ -11,18 +11,26 @@
     </a>
     <ul class="treeview-menu">
 
-    	@php
-    	$count = count($sideMenu->allChildrenMenus);
-    	for($i=0; $i < $count; $i++){
-    	@endphp
-    		<li class="@if(Route::currentRouteName() == $sideMenu->allChildrenMenus[$i]->route_name ) active menu-activated @endif">
-	            <a href="{{route($sideMenu->allChildrenMenus[$i]->route_name)}}">{!! $sideMenu->allChildrenMenus[$i]->icon !!}{{$sideMenu->allChildrenMenus[$i]->name}}</a>
-	        </li>
-	        @php
-    	}
+        @php
+        $count = count($sideMenu->allChildrenMenus);
+        for($i=0; $i < $count; $i++){
+        @endphp
+            <li class="@if(Route::currentRouteName() == $sideMenu->allChildrenMenus[$i]->route_name ) active menu-activated @endif">
+                <a href="{{route($sideMenu->allChildrenMenus[$i]->route_name)}}">{!! $sideMenu->allChildrenMenus[$i]->icon !!}{{$sideMenu->allChildrenMenus[$i]->name}}</a>
+            </li>
+            @php
+        }
         @endphp
 
     </ul>
+</li>
+
+@else
+
+<li class="@if(Route::currentRouteName() == $sideMenu->route_name ) active @endif">
+    <a href="{{route($sideMenu->route_name)}}">
+        <i class="fa fa-users"></i> <span>{{$sideMenu->name}}</span>
+    </a>
 </li>
 
 @endif
